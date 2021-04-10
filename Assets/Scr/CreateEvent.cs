47315737ee2cd7af93f56af731fcffbaf92bec00
314pies <ian121363@gmail.com>
2021-04-10 00:16:11 -0400

    Add API gateway call test

--- Assets/Scr/CreateEvent.cs
+++ Assets/Scr/CreateEvent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Net.Http;
 
 public class CreateEvent : MonoBehaviour
 {
@@ -10,10 +11,24 @@ public class CreateEvent : MonoBehaviour
     TMP_InputField WhenInput, HowManyInput, RestaurentAddressInput;
 
 
-    public void OnCreateButtonClicked()
+    public async void OnCreateButtonClicked()
     {
         Debug.Log(WhenInput.text);
         Debug.Log(HowManyInput.text);
         Debug.Log(RestaurentAddressInput.text);
+
+        using (var httpClient = new HttpClient())
+        {
+            var reqPar = "numPeople=3&time=2021_4_9_12_15_30&restaurantId=12345";
+
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"),
+                "https://uzeqgezy5e.execute-api.us-west-2.amazonaws.com/v1/test1?" + reqPar))
+            {
+                var response = await httpClient.SendAsync(request);
+                Debug.Log(response);
+                string body = await response.Content.ReadAsStringAsync();
+                Debug.Log(body);
+            }
+        }
     }
 }
