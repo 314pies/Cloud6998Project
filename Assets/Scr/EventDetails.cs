c89a36069b2f30368d359d6f804fd939be534211
Davidxcliu <liuxc.office@gmail.com>
2021-04-12 23:51:47 -0400

    no message

--- Assets/Scr/EventDetails.cs
+++ Assets/Scr/EventDetails.cs
@@ -1,6 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Net.Http;
+using Michsky.UI.ModernUIPack;
+
 
 public class EventDetails : MonoBehaviour
 {
@@ -13,10 +18,43 @@ public class EventDetails : MonoBehaviour
     public async void OnDropButtonClicked()
     {
         Debug.Log("OnDropButtonClicked");
+
+        using (var httpClient = new HttpClient())
+        {
+            //var reqPar = "numPeople=3&time=2021_4_9_12_15_30&restaurantId="+SelectedRestaurentID;
+            var reqPar = "eid=2021-04-05_10:28:33.666930" + "&userId=uid1";
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"),
+                "https://333f7sxvgg.execute-api.us-west-2.amazonaws.com/v1/join?" + reqPar))
+            {
+                var response = await httpClient.SendAsync(request);
+                Debug.Log(response);
+                string body = await response.Content.ReadAsStringAsync();
+                Debug.Log(body);
+
+                PopupManager.OpenPopup("API result", body);
+            }
+        }
+
     }
 
     public async void OnJoinButtonClicked()
     {
         Debug.Log("OnJoinButtonClicked");
+
+        using (var httpClient = new HttpClient())
+        {
+            //var reqPar = "numPeople=3&time=2021_4_9_12_15_30&restaurantId="+SelectedRestaurentID;
+            var reqPar = "eid=2021-04-05_10:28:33.666930" + "&userId=uid1";
+            using (var request = new HttpRequestMessage(new HttpMethod("POST"),
+                "https://333f7sxvgg.execute-api.us-west-2.amazonaws.com/v1/drop?" + reqPar))
+            {
+                var response = await httpClient.SendAsync(request);
+                Debug.Log(response);
+                string body = await response.Content.ReadAsStringAsync();
+                Debug.Log(body);
+
+                PopupManager.OpenPopup("API result", body);
+            }
+        }
     }
 }
