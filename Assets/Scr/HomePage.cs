6e2b0115ac0e1d49c79e0073db04a21e098eca06
Davidxcliu <liuxc.office@gmail.com>
2021-04-12 22:59:13 -0400

    no message

--- Assets/Scr/HomePage.cs
+++ Assets/Scr/HomePage.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Http;
+using Michsky.UI.ModernUIPack;
 
 public class HomePage : MonoBehaviour
 {
@@ -11,5 +14,21 @@ public class HomePage : MonoBehaviour
     public async void OnSearchButtonClicked()
     {
         Debug.Log(SearchInput.text);
+
+        using (var httpClient = new HttpClient())
+        {
+            
+            var reqPar = "q=" + SearchInput.text;
+            using (var request = new HttpRequestMessage(new HttpMethod("GET"),
+                "https://333f7sxvgg.execute-api.us-west-2.amazonaws.com/v1/search?" + reqPar))
+            {
+                var response = await httpClient.SendAsync(request);
+                Debug.Log(response);
+                string body = await response.Content.ReadAsStringAsync();
+                Debug.Log(body);
+
+                PopupManager.OpenPopup("API result", body);
+            }
+        }
     }
 }
