e5ea14108f867cf92d13a43a1f666c2d15f7bc33
Davidxcliu <liuxc.office@gmail.com>
2021-04-13 22:04:35 -0400

    no message

--- Assets/Scr/EventDetails.cs
+++ Assets/Scr/EventDetails.cs
@@ -12,7 +12,8 @@ public class EventDetails : MonoBehaviour
     // Start is called before the first frame update
     void Start(eid)
     {
-        var reqPar = "eid=" + eid;
+        eid = eid.Replace(" ", "_");
+        var reqPar = "q=" + eid;
         using (var request = new HttpRequestMessage(new HttpMethod("GET"),
             "https://333f7sxvgg.execute-api.us-west-2.amazonaws.com/v1/detail?" + reqPar))
         {
@@ -28,6 +29,8 @@ public class EventDetails : MonoBehaviour
             string userId = stuff["eventIds"][0]["userId"];
             string restaurantId = stuff["eventIds"][0]["restaurantId"];
             string time = stuff["eventIds"][0]["time"];
+            TimeSpan timeSpan = TimeSpan.FromSeconds(time);
+            string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
             string numPeople = stuff["eventIds"][0]["numPeople"];
             string userName = stuff["eventIds"][0]["userName"];
             string gender = stuff["eventIds"][0]["gender"];
